c94944f94ca010a7007d0cc27442f367a4866c4b
Jon Schneider <jschneider2013@gmail.com>
2018-08-10 02:01:43 -0400

    movement

--- Snake.gd
+++ Snake.gd
@@ -1,30 +1,62 @@
-extends Area2D
+extends Node2D
 
-const SPEED = 100
-var velocity = Vector2()
+export (float) var SPEED = 100
 
+var SnakeCell = preload("res://SnakeCell.tscn") 
 
+var is_game_over = false
+var cells = []
+# cells[0] is back
+# cells[-1] is front
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	var cell = SnakeCell.instance()
+	cell.back_point = Vector2(50, 50)
+	cell.front_point = Vector2(200, 50)
+	add_child(cell)
+	cell.connect("hit", self, "game_over")
+	cells.append(cell)
+	
+	
 
 func _process(delta):
+	if is_game_over:
+		return
 	
+	var dir = direction()
+	var input_dir = dir
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = SPEED
-		velocity.y = 0
+		input_dir = Vector2(1, 0)
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -SPEED
-		velocity.y = 0
+		input_dir = Vector2(-1, 0)
 	elif Input.is_action_pressed("ui_up"):
-		velocity.x = 0
-		velocity.y = -SPEED
+		input_dir = Vector2(0, -1)
 	elif Input.is_action_pressed("ui_down"):
-		velocity.x = 0
-		velocity.y = SPEED
-		
-	position.x += delta*velocity.x
-	position.y += delta*velocity.y
+		input_dir = Vector2(0, 1)
+	
+	var dist = delta*SPEED
+	
+	if abs(dir.dot(input_dir)) < 1e-3:
+		var new_cell = SnakeCell.instance()
+		new_cell.back_point = cells[-1].front_point
+		new_cell.front_point = new_cell.back_point + dist*input_dir
+		add_child(new_cell)
+		new_cell.connect("hit", self, "game_over")
+		cells.append(new_cell)
+	else:
+		cells[-1].grow(dist)
+	
+	while dist > cells[0].length():
+		dist -= cells[0].length()
+		cells[0].queue_free()
+		cells.pop_front()
+	
+	cells[0].shrink(dist)
+	
+func direction():
+	return cells[-1].direction()
 
+func game_over():
+	if not is_game_over:
+		print("oh no we lost")
+		is_game_over = true
--- SnakeCell.gd
+++ SnakeCell.gd
@@ -1,10 +1,9 @@
 extends Area2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+signal hit
 
 export (float) var WIDTH
+export (float) var COLLIDE_MARGIN
 
 var back_point = Vector2(100, 0)
 var front_point = Vector2(500, 0)
@@ -21,19 +20,44 @@ func _process(delta):
 	
 func update_size():
 	var is_horizontal = back_point.y == front_point.y
-	var new_rectangle = []
-	if is_horizontal:
-		new_rectangle.append(Vector2(back_point.x, back_point.y-WIDTH/2))
-		new_rectangle.append(Vector2(back_point.x, back_point.y+WIDTH/2))
-		new_rectangle.append(Vector2(front_point.x, front_point.y+WIDTH/2))
-		new_rectangle.append(Vector2(front_point.x, back_point.y-WIDTH/2))
-	else:
-		assert back_point.x == front_point.x
-		new_rectangle.append(Vector2(back_point.x-WIDTH/2, back_point.y))
-		new_rectangle.append(Vector2(back_point.x+WIDTH/2, back_point.y))
-		new_rectangle.append(Vector2(front_point.x+WIDTH/2, front_point.y))
-		new_rectangle.append(Vector2(front_point.x-WIDTH/2, front_point.y))
-		
-	$Polygon2D.polygon = new_rectangle
-	$CollisionShape2D.shape.points = new_rectangle
 	
+	var left = min(back_point.x, front_point.x) 
+	var right = max(back_point.x, front_point.x)
+	var up = min(back_point.y, front_point.y)
+	var down = max(back_point.y, front_point.y)
+	
+	"""
+	$Polygon2D.polygon = [Vector2(left - WIDTH/2, up - WIDTH/2),
+						 Vector2(left - WIDTH/2, down + WIDTH/2),
+						 Vector2(right + WIDTH/2, down + WIDTH/2),
+						 Vector2(right + WIDTH/2, up - WIDTH/2)]
+	"""
+
+	$CollisionShape2D.shape.points = [Vector2(left + COLLIDE_MARGIN/2, up + COLLIDE_MARGIN/2),
+						 			  Vector2(left + COLLIDE_MARGIN/2, down - COLLIDE_MARGIN/2),
+						 			  Vector2(right - COLLIDE_MARGIN/2, down - COLLIDE_MARGIN/2),
+						 			  Vector2(right - COLLIDE_MARGIN/2, up + COLLIDE_MARGIN/2)]
+	
+	print(shape_owner_get_shape(0, 0))
+	$Polygon2D.polygon = $CollisionShape2D.shape.points
+	
+func grow(dist):
+	# front point to move forward dist
+	front_point = front_point + dist*direction()
+	update_size()
+	
+func shrink(dist):
+	# back point to move forward dist
+	back_point = back_point + dist*direction()
+	update_size()
+	
+func direction():
+	return (front_point - back_point).normalized()
+	
+func length():
+	return (front_point - back_point).length()
+	
+	
+func _on_SnakeCell_area_entered(area):
+	emit_signal("hit")
+	pass # replace with function body
