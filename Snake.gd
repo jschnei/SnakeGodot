4f1da598e75fcd3ebc17851ba8acfe49992e410f
Jon Schneider <jschneider2013@gmail.com>
2018-08-09 23:42:40 -0400

    added basic snake scene

---
+++ Snake.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+const SPEED = 100
+var velocity = Vector2()
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _process(delta):
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_up"):
+		velocity.x = 0
+		velocity.y = -SPEED
+	elif Input.is_action_pressed("ui_down"):
+		velocity.x = 0
+		velocity.y = SPEED
+		
+	position.x += delta*velocity.x
+	position.y += delta*velocity.y
+
